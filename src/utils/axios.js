64bf9b361c7c579c61021b637ad405ff7437caf0
younghope1210 <66733651+younghope1210@users.noreply.github.com>
2024-04-18 00:57:06 +0900

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -12,7 +12,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.PROD ?
-    '' : 'http://localhost:4000'
+    'https://e-commerce-backend-h2s3.onrender.com' : 'http://localhost:4000'
 });
 
 
